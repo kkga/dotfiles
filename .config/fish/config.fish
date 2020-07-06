4c8f4de11d6481bdcf818e982cce4c5c63e3de7d
Gadzhi Kharkharov <me@kkga.me>
2020-07-06 13:33:11 +0300

    update fish configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,5 +21,11 @@ alias godot '/Applications/Godot.app/Contents/MacOS/Godot'
 alias kd 'kitty @ set-colors -a "~/.config/kitty/theme-dark.conf"'
 alias kl 'kitty @ set-colors -a "~/.config/kitty/theme-light.conf"'
 
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
 zoxide init fish | source
 starship init fish | source
--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-Dotfiles approach described here:
-https://www.atlassian.com/git/tutorials/dotfiles
-
-macos and linux configuration files, managed with [yadm](https://github.com/TheLocehiliosan/yadm)
+macos and linux configuration files, managed with [yadm](https://github.com/TheLocehiliosan/yadm).
 
 ![](ss.png)
