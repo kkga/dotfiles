08cfe7d1949707a4d990e1f3dd0eceabf0bb484c
Gadzhi Kharkharov <me@kkga.me>
2020-07-10 20:58:42 +0300

    upd

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,19 +1,11 @@
 set fish_greeting ''
 
 alias vim 'nvim'
-alias nano 'kibi'
-
-alias p 'sudo pacman'
 alias c 'clear'
 alias ls 'exa'
-
+alias eo 'sudo eopkg'
 alias gg 'lazygit'
-
 alias brewup 'brew update && brew upgrade && brew cask upgrade'
-
-alias gdocs 'open ~/repos/godot-docs/_build/html/index.html'
-alias godot '/Applications/Godot.app/Contents/MacOS/Godot'
-
 alias kd 'kitty @ set-colors -a "~/.config/kitty/theme-dark.conf"'
 alias kl 'kitty @ set-colors -a "~/.config/kitty/theme-light.conf"'
 
