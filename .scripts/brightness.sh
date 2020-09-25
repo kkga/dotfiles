079a8623e622e7e256d188bc5d8bb299eaf77c5a
Gadzhi Kharkharov <me@kkga.me>
2020-09-25 18:57:01 +0300

    add brightness script

---
+++ .scripts/brightness.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#----------
+# Adjust external display brightness via ddcutil
+# Pass 2 arguments for 1st and 2nd display
+# Needs i2c_dev kernel module loaded
+# https://wiki.archlinux.org/index.php/Backlight#External_monitors
+
+sudo ddcutil -d 1 setvcp 10 $1
+sudo ddcutil -d 2 setvcp 10 $2
