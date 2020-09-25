19d8203efd65cef4768d467f64149d0590117b33
Gadzhi Kharkharov <me@kkga.me>
2020-09-25 12:40:20 +0300

    upd

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,6 @@
 alias vim 'nvim'
-alias c 'clear'
 alias ls 'exa'
-alias eo 'sudo eopkg'
 alias gg 'lazygit'
-alias brewup 'brew update && brew upgrade && brew cask upgrade'
-alias kd 'kitty @ set-colors -a "~/.config/kitty/theme-dark.conf"'
-alias kl 'kitty @ set-colors -a "~/.config/kitty/theme-light.conf"'
-
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
 
 set fish_greeting
 set fish_color_command normal --bold
@@ -32,6 +21,12 @@ set fish_pager_color_prefix normal --bold
 set fish_pager_color_progress normal --bold
 set fish_pager_color_completion normal
 set fish_pager_color_description $fish_color_comment
+
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
 
 zoxide init fish | source
 starship init fish | source
