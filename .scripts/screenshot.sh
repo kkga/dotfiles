44c8e25ea845015a8e96d6441d7f276ce2618794
Gadzhi Kharkharov <me@kkga.me>
2020-11-06 12:43:49 +0300

    upd

--- .scripts/screenshot.sh
+++ .scripts/screenshot.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-# Simple script that uses shotgun and hacksaw to take a screenshot and upload to 0x0.st
+# Use maim to take a screenshot and upload to 0x0.st
 
 screenshot=$(
-    echo -e "Clipboard\nFile\n0x0.st" | dmenu -p "Screenshot" -l 10
+    echo "Clipboard\nFile\n0x0.st" | dmenu -p "Screenshot" -l 10
 )
 
-selection=$(hacksaw -f "-i %i -g %g")
+# selection=$(slop -f "-i %i -g %g")
 
 case "$screenshot" in
-    Clipboard) shotgun $selection - | xclip -selection clipboard -t image/png;;
-    File) shotgun $selection ~/Pictures/Screenshots/$(date +%s).png;;
-    0x0.st) shotgun $selection ~/tmp/screenshot.png; cd ~/tmp; curl -F'file=@screenshot.png' https://0x0.st | xclip -selection clipboard;;
+    Clipboard) maim --select - | xclip -selection clipboard -t image/png;;
+    File) maim --select ~/Pictures/Screenshots/$(date +%s).png;;
+    0x0.st) maim --select ~/tmp/screenshot.png; cd ~/tmp; curl -F'file=@screenshot.png' https://0x0.st | xclip -selection clipboard;;
 esac
