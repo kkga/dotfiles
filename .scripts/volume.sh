7d894c000f4a3abbb69c50b7894d99d3f568c99f
Gadzhi Kharkharov <me@kkga.me>
2020-11-06 12:02:54 +0300

    upd

--- .scripts/volume.sh
+++ .scripts/volume.sh
@@ -4,13 +4,15 @@
 # Arbitrary but unique message id
 msgId="991049"
 
-amixer set Master $@ > /dev/null
+amixer set Master -M $@ > /dev/null
 
-STATUS=$(amixer sget Master | tail -n1 | sed -r "s/.*\[(.*)\]/\1/")
-VOLUME=$(amixer get Master | tail -n1 | sed -r "s/.*\[(.*)%\].*/\1/")
+STATUS=$(amixer sget Master -M | tail -n1 | sed -r "s/.*\[(.*)\]/\1/")
+VOLUME=$(amixer get Master -M | tail -n1 | sed -r "s/.*\[(.*)%\].*/\1/")
 
 if [ "$STATUS" = "off" ]; then
 	dunstify "Volume" "Mute" -r "$msgId"
 else
 	dunstify "Volume" "$VOLUME" -r "$msgId"
 fi
+
+gsound-play -b alsa -i audio-volume-change -d "changeVolume"
