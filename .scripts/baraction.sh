db0898ac1e3d2ea60daf599d3c47dbf7465e2a73
Gadzhi Kharkharov <me@kkga.me>
2020-11-06 19:25:51 +0300

    upd

--- .scripts/baraction.sh
+++ .scripts/baraction.sh
@@ -4,13 +4,13 @@
 SLEEP_SEC=5
 while :; do
 	# music
-	PLAYER_STATUS=$(playerctl status)
-	PLAYER_ARTIST=$(playerctl metadata artist)
-	PLAYER_TITLE=$(playerctl metadata title)
-	PLAYER_STR=""
-	if [ $PLAYER_STATUS = "Playing" ]; then
-		PLAYER_STR="$PLAYER_ARTIST - $PLAYER_TITLE"
-	fi
+	# PLAYER_STATUS=$(playerctl status)
+	# PLAYER_ARTIST=$(playerctl metadata artist)
+	# PLAYER_TITLE=$(playerctl metadata title)
+	# PLAYER_STR=""
+	# if [ $PLAYER_STATUS = "Playing" ]; then
+	# 	PLAYER_STR="$PLAYER_ARTIST - $PLAYER_TITLE"
+	# fi
 
 	# system
 	CPU=$(sensors | grep Tdie | cut -c 16-17)
@@ -21,7 +21,7 @@ while :; do
 	GPU_INFO_STR="GPU $GPU°/$GPU_MEM°/$GPU_FAN"
 	DATE_STR=`date +"%a %d %b %H:%M"`
 
-	xsetroot -name " $PLAYER_STR  |  $CPU_INFO_STR  |  $GPU_INFO_STR  |  $DATE_STR "
+	xsetroot -name "  $CPU_INFO_STR  |  $GPU_INFO_STR  |  $DATE_STR "
 
 	sleep $SLEEP_SEC
 done
