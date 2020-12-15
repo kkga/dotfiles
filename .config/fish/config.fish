93bab78f18360a452c284f7759a37a5d94b24165
Gadzhi Kharkharov <me@kkga.me>
2020-12-15 15:41:27 +0300

    upd

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,31 +1,4 @@
-set -Ux EDITOR nvim
-set -Ux FZF_DEFAULT_COMMAND 'fd --type f'
-set -Ux FZF_DEFAULT_OPTS '--color=bw --reverse --height=40% --border=horizontal'
-
-set -U fish_user_paths ~/.scripts ~/.local/bin ~/go/bin ~/.cargo/bin $fish_user_paths
-
-alias vim 'nvim'
-alias ls 'exa'
-alias cat 'bat'
-alias gg 'lazygit'
-alias t 'togo'
-alias y 'yadm'
-
-abbr gs 'git status'
-abbr gc 'git checkout'
-abbr gps 'git push'
-abbr gpl 'git pull'
-abbr gl 'git log'
-abbr gd 'git diff'
-abbr gdt 'git difftool'
-
-abbr xbr 'doas xbps-remove'
-abbr xbi 'doas xbps-install'
-abbr xbq 'xbps-query'
-
-set lucid_dirty_indicator '*'
-set lucid_clean_indicator ' '
-set lucid_prompt_symbol '$'
+set -x EDITOR nvim
 
 set fish_greeting
 set fish_color_command normal --bold
@@ -49,5 +22,7 @@ set fish_pager_color_background --background=background
 set fish_pager_color_description $fish_color_comment
 set fish_pager_color_selected_completion white
 set fish_pager_color_selected_description white
+
+bind \cr 'search_history'
 
 zoxide init fish | source
