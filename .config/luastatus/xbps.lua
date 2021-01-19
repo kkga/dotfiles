1924732363e0bb7accec4258ed4724cc5e84fd75
Gadzhi Kharkharov <me@kkga.me>
2021-01-19 12:54:34 +0300

    upd

--- .config/luastatus/xbps.lua
+++ .config/luastatus/xbps.lua
@@ -2,15 +2,12 @@ widget = {
     plugin = "timer",
     opts = {period = 5 * 60},
     cb = function()
-        os.execute("doas xbps-install -S")
+        os.execute("xbps-install -S")
         local f = assert(io.popen("doas xbps-install -un | wc -l"))
         local res = f:read("*all")
-        f.close()
         local output
         if tonumber(res) > 0 then
             output = string.format("⇪ %d", res)
-        else
-            output = nil
         end
         return output
     end
