e550a1ebab8c1a41ece57e794ca7403402a0ce24
Gadzhi Kharkharov <me@kkga.me>
2021-01-21 20:31:27 +0300

    update sway config

--- .local/bin/sway-run.sh
+++ .local/bin/sway-run.sh
@@ -7,5 +7,7 @@ export QT_QPA_PLATFORM=wayland-egl
 export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
 export XDG_SESSION_TYPE=wayland
+export XDG_SESSION_DESKTOP=sway
+export XDG_CURRENT_DESKTOP=sway
 
 exec dbus-run-session sway -d 2> ~/tmp/sway.log
