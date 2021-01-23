fa1369555f74f663a9ad76d1608bb44524470aed
Gadzhi Kharkharov <me@kkga.me>
2021-01-23 16:10:24 +0300

    reorganize env vars

--- .local/bin/power-menu.sh
+++ .local/bin/power-menu.sh
@@ -3,12 +3,11 @@
 # Launch a dmenu based power menu.
 
 selection=$(
-  echo "lock\nkill-x\nshutdown\nreboot\nsuspend" | $DMENU
+  echo "lock\nshutdown\nreboot\nsuspend" | $DMENU
 )
 
 case "$selection" in
        lock) loginctl lock-session;;
-     kill-x) pkill xinit;;
    shutdown) loginctl poweroff;;
      reboot) loginctl reboot;;
     suspend) loginctl suspend & loginctl lock-session;;
