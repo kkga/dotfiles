76bddf4f767c835b2f7f0170b4ae152afa689c2c
Gadzhi Kharkharov <me@kkga.me>
2021-01-24 17:07:16 +0300

    upd

--- .local/bin/luastatus-stdout.sh
+++ .local/bin/luastatus-stdout.sh
@@ -2,5 +2,5 @@
 
 cd /home/kkga/.config/luastatus
 
-luastatus-stdout-wrapper -B separator='+@fg=2; ╱ +@fg=0;' xbps.lua backlight.lua cpu-usage.lua\
-	cpu-temp.lua gpu-temp.lua network-rate.lua xkb.lua time-date.lua
+luastatus-stdout-wrapper -B separator='   ' mail.lua xbps.lua backlight.lua cpu-usage.lua\
+	cpu-temp.lua gpu-temp.lua network-rate.lua weather.lua xkb.lua time-date.lua
