16f37dee8390b2f76655f58168fbc9efef48d597
Gadzhi Kharkharov <me@kkga.me>
2021-01-24 20:45:26 +0300

    upd

--- .local/bin/luastatus-sway.sh
+++ .local/bin/luastatus-sway.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-cd ~/.config/luastatus/sway && exec luastatus-i3-wrapper\
-	backlight.lua\
-	xbps.lua\
-	cpu-usage.lua\
-	alsa.lua\
-	wireless.lua\
-	network-rate.lua\
-	battery.lua\
-	time-date.lua\
+cd ~/.config/luastatus && exec luastatus-i3-wrapper\
+	sway-backlight.lua\
+	sway-mail.lua\
+	sway-xbps.lua\
+	sway-cpu-usage.lua\
+	sway-alsa.lua\
+	sway-wireless.lua\
+	sway-network-rate.lua\
+	sway-battery.lua\
+	sway-time-date.lua\
