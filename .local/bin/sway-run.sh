6b1a921d24236938434f5a0fff12c6d1c4e542ac
Gadzhi Kharkharov <me@kkga.me>
2021-03-18 08:45:03 +0300

    upd

--- .local/bin/sway-run.sh
+++ .local/bin/sway-run.sh
@@ -7,3 +7,5 @@ export XDG_SESSION_DESKTOP=sway
 export XDG_CURRENT_DESKTOP=sway
 
 exec dbus-run-session sway $@
+exec dbus-run-session ~/repos/sway/build/sway/sway $@
+# exec dbus-run-session ~/repos/dwl/dwl $@
