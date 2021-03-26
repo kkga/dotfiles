d554a269462e56af88505a3128b9fd813a7c3924
Gadzhi Kharkharov <me@kkga.me>
2021-03-26 09:00:47 +0300

    upd

--- .config/nvim/lua/lua_config.lua
+++ .config/nvim/lua/lua_config.lua
@@ -6,4 +6,4 @@ require("formatter_config")
 require("gitsigns_config")
 require("treesitter_config")
 
-vim.cmd "au TextYankPost * lua vim.highlight.on_yank {on_visual = false}" -- disabled in visual mode
+vim.cmd "au TextYankPost * lua vim.highlight.on_yank {on_visual = false}"
