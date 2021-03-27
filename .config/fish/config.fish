8a86cd8f35821a560fc616393e5f738e6e71d66d
Gadzhi Kharkharov <me@kkga.me>
2021-03-27 13:58:55 +0300

    upd

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,31 +9,32 @@ alias xr='doas xbps-remove'
 set --global hydro_symbol_prompt '>'
 set --global hydro_symbol_git_dirty '*'
 
-set fish_color_autosuggestion      normal
-set fish_color_cancel              -r
-set fish_color_command             brgreen
-set fish_color_comment             brmagenta
-set fish_color_cwd                 green
-set fish_color_cwd_root            red
-set fish_color_end                 brmagenta
-set fish_color_error               brred
-set fish_color_escape              brcyan
-set fish_color_history_current     --bold
-set fish_color_host                normal
-set fish_color_match               --background=brblue
-set fish_color_normal              normal
-set fish_color_operator            cyan
-set fish_color_param               brblue
-set fish_color_quote               yellow
-set fish_color_redirection         bryellow
-set fish_color_search_match        'bryellow' '--background=brblack'
-set fish_color_selection           'white' '--bold' '--background=brblack'
-set fish_color_status              red
-set fish_color_user                brgreen
-set fish_color_valid_path          --underline
-set fish_pager_color_prefix normal
-set fish_pager_color_completion normal
-set fish_pager_color_description yellow
-set fish_pager_color_selected_background -r
+set fish_greeting					       ''
+set fish_color_autosuggestion              normal
+set fish_color_cancel                      -r
+set fish_color_command                     brgreen
+set fish_color_comment                     brmagenta
+set fish_color_cwd                         green
+set fish_color_cwd_root                    red
+set fish_color_end                         brmagenta
+set fish_color_error                       brred
+set fish_color_escape                      brcyan
+set fish_color_history_current             --bold
+set fish_color_host                        normal
+set fish_color_match                       --background=brblue
+set fish_color_normal                      normal
+set fish_color_operator                    cyan
+set fish_color_param                       brblue
+set fish_color_quote                       yellow
+set fish_color_redirection                 bryellow
+set fish_color_search_match                'bryellow' '--background=brblack'
+set fish_color_selection                   'white' '--bold' '--background=brblack'
+set fish_color_status                      red
+set fish_color_user                        brgreen
+set fish_color_valid_path                  --underline
+set fish_pager_color_prefix                normal
+set fish_pager_color_completion            normal
+set fish_pager_color_description           yellow
+set fish_pager_color_selected_background   -r
 
 zoxide init fish | source
