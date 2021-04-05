391a21be8751fcf3f6b19c1cb4946560e729229c
Gadzhi Kharkharov <me@kkga.me>
2021-04-05 23:42:18 +0300

    random

--- .local/bin/luastatus-sway.sh
+++ .local/bin/luastatus-sway.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-cd ~/.config/luastatus && exec luastatus-i3-wrapper\
-	sway-backlight.lua\
-	sway-cpu-usage.lua\
-	sway-alsa.lua\
-	sway-network.lua\
-	sway-network-rate.lua\
-	sway-kb.lua\
-	sway-battery.lua\
-	sway-time-date.lua\
+cd ~/.config/luastatus && exec luastatus-i3-wrapper \
+    sway-backlight.lua \
+    sway-cpu-usage.lua \
+    sway-alsa.lua \
+    sway-network.lua \
+    sway-network-rate.lua \
+    sway-kb.lua \
+    sway-battery.lua \
+    sway-time-date.lua
