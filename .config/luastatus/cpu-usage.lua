3e49c7e6d8e294fd329d4fc1ffd37fdb6a468770
Gadzhi Kharkharov <me@kkga.me>
2021-05-02 13:42:26 +0300

    remove old configs

--- .config/luastatus/cpu-usage.lua
+++
@@ -1,7 +0,0 @@
-widget = luastatus.require_plugin('cpu-usage-linux').widget{
-    cb = function(usage)
-        if usage ~= nil then
-            return string.format('%5.1f%%', usage * 100)
-        end
-    end,
-}
