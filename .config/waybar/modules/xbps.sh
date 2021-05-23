24c254cc68032bf6ee05150b2cfb634b39ce99ba
Gadzhi Kharkharov <me@kkga.me>
2021-05-24 01:04:54 +0300

    upd

--- .config/waybar/modules/xbps.sh
+++ .config/waybar/modules/xbps.sh
@@ -1,11 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
-doas xbps-install -S > /dev/null
+refresh() {
+	doas xbps-install -S > /dev/null
+}
 
-UPDATES=$(xbps-install -un | cut -d' ' -f2 | sort | uniq -c | xargs)
+check() {
+	UPDATES=$(xbps-install -un | cut -d' ' -f2 | sort | uniq -c | xargs)
+	if [ -z "$UPDATES" ]; then
+		echo ""
+	else
+		echo "$UPDATES""s"
+	fi
+}
 
-if [ -z "$UPDATES" ]; then
-	echo ""
-else
-	echo "$UPDATES""s"
-fi
+while true; do
+	refresh
+	check
+	sleep 10s
+done
