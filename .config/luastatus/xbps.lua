189bf523fb137aad085912c7c587244a62ad9d8b
Gadzhi Kharkharov <me@kkga.me>
2021-06-08 23:07:38 +0300

    upd

--- .config/luastatus/xbps.lua
+++
@@ -1,7 +0,0 @@
-widget = luastatus.require_plugin('file-contents-linux').widget{
-    filename = '/tmp/updates',
-    cb = function(f)
-        -- show the first line of the file
-        return {full_text = f:read('*line')}
-    end,
-}
