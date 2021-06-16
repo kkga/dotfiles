9b1d4689cb368f9a8535407dfa72b614fc79db2a
Gadzhi Kharkharov <me@kkga.me>
2021-06-17 00:03:24 +0300

    upd

--- .config/todoman/config.py
+++ .config/todoman/config.py
@@ -1,8 +1,7 @@
-# A glob expression which matches all directories relevant.
-path = "~/.local/share/vdirsyncer/fastmail/41e0d7df-c8cc-4dd4-80f0-e906eb245141/"
+path = "~/.local/share/vdirsyncer/fastmail/*"
 date_format = "%Y-%m-%d"
 time_format = "%H:%M"
-default_list = "41e0d7df-c8cc-4dd4-80f0-e906eb245141"
+default_list = "default"
 default_due = 48
 humanize = True
-color = "always"
+color = "auto"
