559e240aacb964e8651edb961c7e3c275ff5f0c6
Gadzhi Kharkharov <me@kkga.me>
2021-06-30 23:56:29 +0300

    upd

--- .config/todoman/config.py
+++ .config/todoman/config.py
@@ -1,7 +1,7 @@
-path = "~/.local/share/vdirsyncer/fastmail/*"
+path = "~/.local/share/vdirsyncer/migadu/*"
 date_format = "%Y-%m-%d"
 time_format = "%H:%M"
-default_list = "default"
+default_list = "Home"
 default_due = 48
 humanize = True
 color = "auto"
