e46ba7f93d44c57bd662abee144cbaabe2bf9dbc
Gadzhi Kharkharov <me@kkga.me>
2021-07-01 10:03:02 +0300

    upd

--- .config/todoman/config.py
+++ .config/todoman/config.py
@@ -1,7 +1,7 @@
 path = "~/.local/share/vdirsyncer/migadu/*"
 date_format = "%Y-%m-%d"
 time_format = "%H:%M"
-default_list = "Home"
+default_list = "Tasks"
 default_due = 48
 humanize = True
 color = "auto"
