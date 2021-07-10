ec60d6d23c90d989f0fc8fde24d03e38148fdecf
Gadzhi Kharkharov <me@kkga.me>
2021-07-10 13:10:52 +0300

    upd

--- .config/fish/functions/man.fish
+++ .config/fish/functions/man.fish
@@ -1,8 +1,15 @@
-function man --wraps=kak --description 'use kakoune as manpager'
+function man --wraps=kak --description 'view manpage in kakoune'
     if test (count $argv) -lt 2
         set page "$argv[1]"
     else
         set page "$argv[2]($argv[1])"
     end
-    kak -e "map global normal q :quit<ret>; map global normal m ':enter-user-mode man-mode<ret>'; man $page; set buffer readonly true";
+    kak -e "\
+        map global normal d <c-d>;\
+        map global normal u <c-u>;\
+        map global normal q :quit<ret>;\
+        map global normal m ':enter-user-mode man-mode<ret>';\
+        man $page;\
+        set buffer readonly true\
+    ";
 end
